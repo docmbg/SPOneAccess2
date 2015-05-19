7fb2d740e2286a52ae42ed63047de62f945c7cbe
Martina Dzhibirska <martina.dzhibirska@hp.com>
2015-05-19 10:29:11 +0300

    changes to be saved

--- js/massDelete.js
+++ js/massDelete.js
@@ -1,4 +1,5 @@
 var ep=new ExcelPlus();
+var usersEmails = [];
 // we call openLocal() and when the file is loaded then we want to display its content
 // openLocal() will use the FileAPI if exists, otherwise it will use a Flash object
 ep.openLocal({
@@ -8,23 +9,77 @@ ep.openLocal({
   // show the content of the first sheet
   var initialArray = ep.selectSheet('MassDelete').readAll();
   
-  // extracting the users' emails and pushing them into a newly created userNames array
-  var userNames = [];
+  // extracting the users' emails and pushing them into a newly created usersEmails array
+  //var usersEmails = [];
   for (var i=0; i < initialArray.length; i++) {
  
     for (var j=0; j < initialArray[i].length; j++) {
-     userNames.push(initialArray[i][j]);
+     usersEmails.push(initialArray[i][j]);
     }
     
   }
-  console.log(userNames);
+ 
+  console.log(usersEmails);
 })
 
-var userObject = {
-  loginNames = [],
-  emails = []
+  //Class for user
 
-};
 
-userObject.emails = userNames;
-console.log("This is a property" + userObject.emails);
+var User = (function() {
+    // Create a store to hold the private objects.
+    var privateStore = {};
+    var uid = 0;
+
+    function User() {
+        privateStore[this.id = uid++] = {};
+        privateStore[this.id]._name = "No name";
+        privateStore[this.id]._email = "No email specified";
+        privateStore[this.id]._login = "No login name";
+    }
+
+    User.prototype.setName = function(name) {
+        privateStore[this.id]._name = name;
+    };
+
+    User.prototype.getName = function() {
+        return privateStore[this.id]._name;
+    };
+
+    User.prototype.setLogin = function(login) {
+        privateStore[this.id]._login = login;
+    };
+
+    User.prototype.getLogin = function() {
+        return privateStore[this.id]._login;
+    };
+
+    User.prototype.setEmail = function(email) {
+        privateStore[this.id]._email = email;
+    };
+
+    User.prototype.getEmail = function() {
+        return privateStore[this.id]._email;
+    };
+
+    User.prototype.toString = function userToString(){
+        var ret = 'Name: ' + privateStore[this.id]._name + "\n" +
+                  'Email: ' + privateStore[this.id]._email + "\n" +
+                  'Login Name: ' + privateStore[this.id]._login;
+        return ret;
+    };
+
+    return User;
+}());
+
+
+
+ //Getting the login usename 
+
+  for (var i = 0; i < usersEmails.length; i++) {
+      $SP().people(usersEmails[i], function(people) {
+        if (typeof people === "string") {
+          alert(people); // there was a problem so we prompt it
+        } else
+        for (var i=0; i < people.length; i++) console.log(people[i]+" = "+people[people[i]]);
+    });
+  };
