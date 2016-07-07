55929eaec4af4477b46e6df87cca60e3e71da04e
ES ABM Document Management <document.management@hp.com>
2016-07-07 15:56:30 +0300

    Add files via upload

---
+++ js/getSubSitesWorker.js
@@ -0,0 +1,9 @@
+importScripts("https://rawgit.com/docmbg/SPOneAccess2/1.2.2/js/models.js");
+importScripts("https://rawgit.com/docmbg/SPOneAccess2/1.2.0/js/spgrind.js");
+
+    // because its type is javascript/worker.
+    self.onmessage = function(e) {
+        console.log('Gathering Subsites');
+        var sites = SPGrind.fn.getSPSites(e.data[0], e.data[1], false); //gotovi siteove!!!
+        self.postMessage([sites]);
+    };
